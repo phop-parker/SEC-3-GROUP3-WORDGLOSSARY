85f9da4697f72a14a408e8e8b78400f317e860e1
phxsrp <phxsrp@gmail.com>
2022-04-09 23:31:18 +0700

    add aboutus with ugly css

---
+++ src/components/MemberList.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+    membersList: {
+        type: Array,
+        require: true
+    },
+})
+
+</script>
+ 
+<template>
+    <div>
+        <h2>GROUP 3 FROM SEC 3</h2>
+        <div class="cards">
+        <div class="card" v-for="(member, index) in membersList" :key="index">
+            <img :src="member.img" :alt="member.studentId" style="width:100%" />
+            <div class="container">
+                <h4>
+                    <b>{{ member.name }} {{ member.surname }}</b>
+                </h4>
+                <p>{{ member.studentId }}</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+ 
+<style scoped>
+.cards{
+        display: flex;
+      justify-content: center;
+}
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    width: 20%;
+    margin: 4px;
+
+
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+    padding: 2px 16px;
+}
+</style>
---
+++ src/views/AboutUs.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MemberList from "../components/MemberList.vue"
+const members = [
+{ name: "Sarida", surname: "Buakaew", studentId: "63130500112", img: "../src/assets/63130500112.png" },
+{ name: "Sarisa", surname: "Phulakorn", studentId: "63130500116", img: "../src/assets/63130500116.png" },
+{ name: "Siraphop", surname: "Pattaragatewit", studentId: "63130500119", img: "../src/assets/63130500119.png" },
+{ name: "Supichaya", surname: "Muennunch", studentId: "63130500123", img: "../src/assets/63130500123.png" },
+{ name: "Thanat", surname: "Leeruengkij", studentId: "63130500156", img: "../src/assets/63130500156.png" }]
+
+</script>
+ 
+<template>
+<MemberList :membersList="members"/>
+</template>
+ 
+<style>
+</style>
